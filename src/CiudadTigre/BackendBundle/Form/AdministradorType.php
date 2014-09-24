25a0f5f8ccf967653f5ef9b15f697ba0dad2ff8a
Bell Mundarain <bmundarain@gmail.com>
2014-09-24 15:51:42 -0430

    Common updates

---
+++ src/CiudadTigre/BackendBundle/Form/AdministradorType.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace CiudadTigre\BackendBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AdministradorType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nombre')
+            ->add('login')
+            ->add('password')
+            ->add('salt')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CiudadTigre\AnuncianteBundle\Entity\Administrador'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'ciudadtigre_backendbundle_administrador';
+    }
+}
